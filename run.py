2aef31c1602aa01ebddcc012e8dab2cb82f08b60
ronaks9065 <ronak.prajapati@caxsol.com>
2022-12-01 16:51:24 +0530

    First Commit

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
-from app import app
-import os
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<h2>Hello, World!</h2>'
 
 if __name__ == '__main__':
-    app.secret_key = os.urandom(12)
-    app.run()
+    app.run(host='0.0.0.0', port=3000, debug=True)
