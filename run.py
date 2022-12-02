f9f7e847ed3164a46dafc022b4f078b027573198
ronaks9065 <ronak.prajapati@caxsol.com>
2022-12-02 14:47:28 +0530

    5 Commit

--- run.py
+++ run.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h2>Python App Up and Running..</h2>'
+    return '<h1>Python App Up and Running..</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
