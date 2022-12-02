a34d098b6298cb7377285e34df15fbadb0a4d69f
ronaks9065 <115705180+ronaks9065@users.noreply.github.com>
2022-12-02 15:10:01 +0530

    Update application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd /home/ubuntu/PythonApp/
-sudo /home/ubuntu/PythonApp/sonar-scanner/sonar-scanner-4.7.0.2747-linux/bin
-sudo pm2 stop run.py --interpreter python3
+sudo /home/ubuntu/PythonApp/sonar-scanner/sonar-scanner-4.7.0.2747-linux/bin/sonar-scanner
+sudo pm2 stop run.py --interpreter python3 
 sudo pm2 start run.py --interpreter python3
 
 
