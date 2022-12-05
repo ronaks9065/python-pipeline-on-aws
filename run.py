6d66a86a3bcf899d351cc4b8b2d0d465479f785a
ronaks9065 <115705180+ronaks9065@users.noreply.github.com>
2022-12-05 14:31:49 +0530

    Update run.py

--- run.py
+++ run.py
@@ -11,3 +11,157 @@ def hello_world():
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3000, debug=True)
+    
+    from flask import Flask
+from flask import Flask
+app = Flask(__name__)
+
+app = Flask(__name__)
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<h1>Python App Up and Running.. R</h1>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
+    
+    from flask import Flask
+from flask import Flask
+app = Flask(__name__)
+
+app = Flask(__name__)
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<h1>Python App Up and Running.. R</h1>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
+    
+    from flask import Flask
+from flask import Flask
+app = Flask(__name__)
+
+app = Flask(__name__)
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<h1>Python App Up and Running.. R</h1>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
+    
+    from flask import Flask
+from flask import Flask
+app = Flask(__name__)
+
+app = Flask(__name__)
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<h1>Python App Up and Running.. R</h1>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
+    
+    from flask import Flask
+from flask import Flask
+app = Flask(__name__)
+
+app = Flask(__name__)
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<h1>Python App Up and Running.. R</h1>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
+    
+    from flask import Flask
+from flask import Flask
+app = Flask(__name__)
+
+app = Flask(__name__)
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<h1>Python App Up and Running.. R</h1>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
+    
+    from flask import Flask
+from flask import Flask
+app = Flask(__name__)
+
+app = Flask(__name__)
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<h1>Python App Up and Running.. R</h1>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
+    
+    from flask import Flask
+from flask import Flask
+app = Flask(__name__)
+
+app = Flask(__name__)
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<h1>Python App Up and Running.. R</h1>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
+    
+    from flask import Flask
+from flask import Flask
+app = Flask(__name__)
+
+app = Flask(__name__)
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<h1>Python App Up and Running.. R</h1>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
+    
+    from flask import Flask
+from flask import Flask
+app = Flask(__name__)
+
+app = Flask(__name__)
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<h1>Python App Up and Running.. R</h1>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
+    
+    from flask import Flask
+from flask import Flask
+app = Flask(__name__)
+
+app = Flask(__name__)
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return '<h1>Python App Up and Running.. R</h1>'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=3000, debug=True)
